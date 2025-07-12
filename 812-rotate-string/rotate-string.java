4a0d93bd288ba4f26974b06e41259e05057d6dde
Hemanth-Varma-04 <hemanthvarma4114@gmail.com>
2025-07-12 19:28:23 +0530

    Time: 2 ms (11.57%) | Memory: 42.1 MB (13.65%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,5 +1,11 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        return (s.length()==goal.length()&&(s+s).contains(goal));
+        for(int i=0;i<s.length();i++){
+            String rotated = s.substring(i)+s.substring(0,i);
+            if(rotated.equals(goal)){
+                return true;
+            }
+        }
+        return false;
     }
 }
