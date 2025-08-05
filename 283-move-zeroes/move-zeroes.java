9302550fe4b3e0c40b5a455f0d8b7bdb9f00a864
Hemanth-Varma-04 <hemanthvarma4114@gmail.com>
2025-08-05 11:22:19 +0530

    Time: 2 ms (80.02%) | Memory: 46.4 MB (22.44%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int pos = 0;
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                nums[pos] = nums[i];
+                pos++;
+            }
+        }
+        while(pos<n){
+            nums[pos] = 0;
+            pos++;
+        }
+    }
+}
