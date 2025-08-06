c5e89c0920c1b52accc511d19b7d692fd9a14e97
Hemanth-Varma-04 <hemanthvarma4114@gmail.com>
2025-08-06 09:58:35 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (30.63%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,16 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        Map<Integer,Integer> hm = new HashMap<>();
-        for(int i=0;i<n;i++){
-            int compliment = target-nums[i];
-            if(hm.containsKey(compliment)){
-                return new int[] {hm.get(compliment),i};
+        int n=nums.length;
+        for(int i=1;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(nums[j]+nums[j-i]==target){
+                    return new int[]{j,j-i};
+                }
             }
-            else{
-                hm.put(nums[i],i);
-            }
-        } 
-        throw new IllegalArgumentException("No match");
+        }
+
+        return null;
     }
 }
